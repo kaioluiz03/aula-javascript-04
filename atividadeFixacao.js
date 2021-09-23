2fa24760ff1bf8d879b77eab7a60c66b0edaa5b9
Kaio <luizkaio9815@gmail.com>
2021-09-23 18:52:47 -0300

    Envio a primeira atividade do dia 23/09

---
+++ atividadeFixacao.js
@@ -0,0 +1,39 @@
+//1)Criar Array com 10 itens ( de 1 a 10).
+//a)Exibir no console o total de itens.
+//b)Exibir no console os itens nas posições: 1, 3, 5, 8.
+
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//console.log(numeros.length);
+//console.log(numeros[1], numeros[3], numeros[5], numeros[8]);
+
+//  //  //  //  //  //  //  //  //  //  //  //
+
+/*2)Criar Array com 5 itens (nome de filmes).
+  a)Exibir no console o total de itens.
+  b)Exibir no console os itens nas posições: 2, 4.
+  c)Exibir no console o item na primeira posição.*/
+
+var filmes = ['THOR: AMOR E TROVÃO.', 'ESCAPE ROOM 2: TENSÃO MÁXIMA.', 'VIÚVA NEGRA.', 'UM PRÍNCIPE EM NOVA YORK.', 'O LAR DAS CRIANÇAS PECULIARES.']
+
+//console.log(filmes.length);
+//console.log("O filme na posição 2 é " + filmes[2] + " E o filme na posição 4 é " +  filmes[4]);
+//console.log(filmes[0]);
+
+//  //  //  //  //  //  //  //  //  //  //  //
+
+/*3)Criar Array com 6 itens (nome de músicas).
+  a)Exibir no console o total de itens.
+  b)Exibir no console os itens nas posições : 1, 2, 6, 8.
+  c)Exibir no console o item na última posição.*/
+
+var theWarning = ['EVOLVE', 'Runaway', 'CHOKE', 'Copper Bullets', 'Show Me the Light', 'Survive'];
+var ultima = theWarning[theWarning.length -1];
+
+//console.log(theWarning.length);
+//console.log(theWarning[1]);
+//console.log(theWarning[2]);
+//console.log(theWarning[6]);
+//console.log(theWarning[8]);
+//console.log("A música na última posição é " + ultima);
+//  //  //  //  //  //  //  //  //  //  //  //
